67aaa9c2ef69c30ace4b39d6b8e51de75ee21269
CroclingJ09 <corentin.jenni@gmail.com>
2024-11-15 15:09:39 +0100

    Add Walljump animation

--- src/kaplayCtx.js
+++ src/kaplayCtx.js
@@ -13,14 +13,17 @@ export const k = kaboom({
 
 k.loadSprite("player", "./sprites/Player.png", {
     sliceX: 10,
-    sliceY: 6,
+    sliceY: 7,
     anims: {
         idle: { from: 0, to: 9, loop: true},
         walk: { from: 10, to: 17, loop: true},
         run: { from: 20, to: 27, loop: true},
         jump: { from: 30, to: 35},
-        doubleJump: { from: 40, to: 45}
+        doubleJump: { from: 40, to: 45},
+        fall: { from: 33, to: 35},
+        wall: {from: 60, to: 62, loop:true}
     }
 })
 
 k.loadSprite("testRoom", "./maps/test.png")
+k.loadSprite("wallJump", "./maps/wallJump.png")
