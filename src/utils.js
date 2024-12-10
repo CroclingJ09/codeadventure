f369e210c7af40935dbcdeecc95497d0a6fdcf31
Corentin Jenni <corentin.jenni@gmail.com>
2024-12-10 15:56:28 +0100

    added pause screen

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,18 @@
 export function pauseGame(k,player){
+    let pausescreen = document.getElementById("pause")
+    console.log(pausescreen)
     if (k.paused === false){
         console.log("pause")
         k.paused= true
         player.paused = true
+        pausescreen.classList.add("paused")
+        pausescreen.classList.remove("unpaused")
     }
     else if (k.paused === true){
         console.log("play")
         k.paused= false
         player.paused = false
+        pausescreen.classList.remove("paused")
+        pausescreen.classList.add("unpaused")
     }
 }
