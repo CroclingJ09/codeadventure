b89f6f1bd109f6d3afaf793f29b8e61d63ec774a
Corentin Jenni <corentin.jenni@gmail.com>
2025-01-16 20:21:19 +0100

    added interactions zone

--- src/entities/interactZones.js
+++ src/entities/interactZones.js
@@ -1,4 +1,5 @@
 import {makePlayer} from "./player.js";
+import {createInteractPopup} from "../utils.js";
 
 export function makeInteractZone(k, pos) {
     const interactZone = k.make([
@@ -16,8 +17,14 @@ export function makeInteractZone(k, pos) {
     ])
 
     interactZone.onCollideUpdate("player", (player) => {
-        // player.interactHandler()
-        console.log("Interaction Zone")
+        // interactHandler(k, player)
+        if (k.isKeyPressed("c") && player.isGrounded()) {
+            if (player.paused === false){
+                k.paused = true
+                player.paused = true
+                createInteractPopup(k, player)
+            }
+        }
     })
 
     return interactZone
