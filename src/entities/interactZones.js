318762785e02cb78c719ed1943036dba8e60c49f
Corentin Jenni <corentin.jenni@gmail.com>
2025-01-25 20:56:07 +0100

    added class to dropZone

--- src/entities/interactZones.js
+++ src/entities/interactZones.js
@@ -1,14 +1,14 @@
 import {makePlayer} from "./player.js";
 import {createInteractPopup} from "../utils.js";
-import {createDropZone} from "../state/dragAndDropManager.js";
+// import {createDropZone} from "../state/dragAndDropManager.js";
 
-export function makeInteractZone(k, pos) {
+export function makeInteractZone(k, name) {
     const interactZone = k.make([
         k.sprite("player", {anim: "wall"}),
         k.area({
             shape: new k.Rect(k.vec2(0,0), 32, 32)
         }),
-        k.pos(pos),
+        k.pos(),
         {
             setPosition(x, y) {
                 this.pos.x = x
@@ -21,10 +21,13 @@ export function makeInteractZone(k, pos) {
         // interactHandler(k, player)
         if (k.isKeyPressed("c") && player.isGrounded()) {
             if (player.paused === false){
+                const dropZone = document.getElementById("drop-zone")
+                dropZone.classList.add(name)
+                console.log(name)
                 k.paused = true
                 player.paused = true
                 createInteractPopup(k, player)
-                createDropZone()
+                // createDropZone(name)
             }
         }
     })
