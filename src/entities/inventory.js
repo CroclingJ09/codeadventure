03719a5047807530977db762f33e04c768165d4f
Corentin Jenni <corentin.jenni@gmail.com>
2025-01-25 20:56:51 +0100

    updated variables for function added class to inventory-objects

--- src/entities/inventory.js
+++ src/entities/inventory.js
@@ -7,24 +7,27 @@ export function createInventory(){
     return inventory
 }
 
-export function updateInventory(inventory, object, objectID){
+export function updateInventory(inventory, object, objectID, k, player){
     let imgLink = ""
+    let objectType = ""
     console.log(object)
     switch (object){
         case "walace":
             imgLink = "public/sprites/Bush.png"
+            objectType = "bush"
             break;
         case "divBlock":
             imgLink = "public/sprites/objects/divBloc.png"
+            objectType = "div-block"
     }
     console.log(imgLink)
     inventory.push(objectID)
     const inventoryBlock = document.getElementById("inventaire")
     inventoryBlock.innerHTML += `
-        <div class="inventory-object" id="` + objectID +`" draggable="true">
+        <div class="inventory-object ` + objectType + `" id="` + objectID +`" draggable="true">
            <img class="object-image" src="`+ imgLink +`" alt="object" draggable="false">
         </div>`
-    addDragAndDrop(objectID)
+    addDragAndDrop(objectID, k, player)
 }
 
 export {inventory}
