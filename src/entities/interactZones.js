3cd964436311652b6f3d88f1c58526d504906d5d
Corentin Jenni <corentin.jenni@gmail.com>
2025-01-28 16:49:44 +0100

    changed way to transfer current interactZone name

--- src/entities/interactZones.js
+++ src/entities/interactZones.js
@@ -1,7 +1,8 @@
 import {makePlayer} from "./player.js";
 import {createInteractPopup} from "../utils.js";
+import {addDragAndDrop} from "../state/dragAndDropManager.js";
 // import {createDropZone} from "../state/dragAndDropManager.js";
-
+export let currInteractZone = null
 export function makeInteractZone(k, name) {
     const interactZone = k.make([
         k.sprite("player", {anim: "wall"}),
@@ -21,16 +22,19 @@ export function makeInteractZone(k, name) {
         // interactHandler(k, player)
         if (k.isKeyPressed("c") && player.isGrounded()) {
             if (player.paused === false){
-                const dropZone = document.getElementById("drop-zone")
-                dropZone.classList.add(name)
+                // const dropZone = document.getElementById("drop-zone")
+                // dropZone.classList.add(name)
+                currInteractZone = name
                 console.log(name)
                 k.paused = true
                 player.paused = true
                 createInteractPopup(k, player)
                 // createDropZone(name)
+                addDragAndDrop(k, player)
             }
         }
     })
 
     return interactZone
+
 }
