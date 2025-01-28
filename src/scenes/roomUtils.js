f9c9f3bd198bf2357a7c012953f56144a3feff75
Corentin Jenni <corentin.jenni@gmail.com>
2025-01-28 16:51:00 +0100

    added function to add DivBlock to canvas

--- src/scenes/roomUtils.js
+++ src/scenes/roomUtils.js
@@ -35,3 +35,22 @@ export function setMapColliders(k, map, colliders) {
         ])
     }
 }
+
+export function addDivBlock(k, addedColliders, addedcolliderName){
+    console.log("yeah")
+    for (const addedCollider of addedColliders){
+        if (addedCollider.name === addedcolliderName){
+            k.add([
+                k.pos(addedCollider.x, addedCollider.y),
+                k.area({
+                    shape: new k.Rect(k.vec2(0), addedCollider.width, addedCollider.height),
+                    collisionIgnore: ["collider"]
+                }),
+                // évite d'être soumis à la gravité (les objects le sont par défaut
+                k.body({isStatic: true}),
+                "collider",
+                addedCollider.type,
+            ])
+        }
+    }
+}
