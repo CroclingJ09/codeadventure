0b3b24b60a7f572c2fd3e3a9e51b346a060fc16e
Corentin Jenni <corentin.jenni@gmail.com>
2025-02-10 22:31:54 +0100

    added the divBlock spawning method added health bar and level name card added function to go to the next level

--- src/scenes/tutorial.js
+++ src/scenes/tutorial.js
@@ -1,18 +1,15 @@
-import {addedColliders} from "./testRoom.js";
 import {setMapColliders} from "./roomUtils.js";
 import {makePlayer} from "../entities/player.js";
-import {state} from "../state/globalStateManager.js";
-import {makeJumpPowerUp} from "../entities/jumpPowerUp.js";
-import {makeDashPowerUp} from "../entities/dashPowerUp.js";
+import {state, statePropsEnum} from "../state/globalStateManager.js";
 import {makeCheckpoints} from "../entities/checkpoints.js";
-import {inventory} from "../entities/inventory.js";
+import {inventory, resetInventory} from "../entities/inventory.js";
 import {makeDivZone} from "../entities/DivBlocks.js";
-import {makeHrefKey} from "../entities/hrefKeys.js";
-import {makeTeleporters} from "../entities/teleporters.js";
 import {makeInteractZone} from "../entities/interactZones.js";
 import {focusOnCanvas, pauseGame, setCameraBorders} from "../utils.js";
-
+import {makeHealthBar} from "../entities/healthbar.js";
+import {makeLevelName} from "../entities/levelName.js";
 export function tutorial(k, roomData) {
+    let addedColliders = []
     k.add([
         k.sprite("tutorial-background", {anim: "play"}),
         k.scale(2.5),
@@ -39,13 +36,17 @@ export function tutorial(k, roomData) {
         if (layer.name === "Collisions"){
             colliders.push(...layer.objects)
         }
+
+        if (layer.name === "AddedCollisions"){
+            addedColliders.push(...layer.objects)
+        }
     }
 
     setMapColliders(k, map, colliders)
 
     const player = map.add(makePlayer(k))
 
-    setCameraBorders(290, 1310, 0, 320, player, k)
+    setCameraBorders(290, 1820, 0, 320, player, k)
 
     for (const position of positions){
         if (position.type === "Text"){
@@ -65,12 +66,6 @@ export function tutorial(k, roomData) {
             else{
                 player.setPosition(state.current().RespawnPositionX, state.current().RespawnPositionY)
             }
-            player.setControls()
-            player.setEvents("tutorial")
-            player.enablePassthrough()
-            player.respawnIfOutOfBounds(600, "tutorial")
-            player.dashHandler()
-            player.spikeHandler()
             // player.enableDoubleJump()
             // player.wallJumpHandler()
         }
@@ -80,13 +75,34 @@ export function tutorial(k, roomData) {
             checkpoint.setPosition(position.x, position.y)
         }
 
+        if (position.type === "DivBlock"){
+            let inventoryCheck = inventory.includes(position.name)
+            console.log(position.name)
+            console.log(inventoryCheck)
+            if (inventoryCheck === false){
+                const divBlock = map.add(makeDivZone(k,position.name))
+                divBlock.setPosition(position.x, position.y)
+            }
+        }
+
         if(position.type === "InteractZone"){
-            const interactZone = map.add(makeInteractZone(k, position.name, map))
+            const interactZone = map.add(makeInteractZone(k, position.name, map, addedColliders))
             interactZone.setPosition(position.x, position.y)
         }
     }
 
     map.add([k.pos(0,0), k.sprite("tutorial-front")])
+
+    const healthbar = map.add(makeHealthBar(k))
+
+    const levelName = map.add(makeLevelName(k, "Tutorial"))
+
+    player.setControls()
+    player.setEvents("tutorial")
+    player.enablePassthrough()
+    player.respawnIfOutOfBounds(600, "tutorial")
+    player.dashHandler()
+    player.spikeHandler(healthbar)
 
     k.onUpdate(() => {
         if (k.isKeyPressed("escape")){
@@ -95,6 +111,15 @@ export function tutorial(k, roomData) {
         else if (player.airDashUnlocked === true){
             player.enableAirDash()
         }
+    })
+
+    player.onCollide("NextZone", () => {
+        k.debug.log("next zone")
+        resetInventory(inventory)
+        state.set(statePropsEnum.playerHp, 3)
+        state.set(statePropsEnum.RespawnPositionX, null)
+        state.set(statePropsEnum.RespawnPositionY, null)
+        k.go("level1")
     })
 
     document.addEventListener("click", focusOnCanvas)
