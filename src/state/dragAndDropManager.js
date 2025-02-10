972841f8c277f964894ecda9e7b8ec4ec6cb7fc6
Corentin Jenni <corentin.jenni@gmail.com>
2025-02-10 22:26:59 +0100

    added onDrop cases for the interaction zones of the first level

--- src/state/dragAndDropManager.js
+++ src/state/dragAndDropManager.js
@@ -1,10 +1,9 @@
 import {focusOnCanvas, playAfterRightDrop} from "../utils.js";
 import {currInteractZone} from "../entities/interactZones.js";
 import {inventory, removeItem} from "../entities/inventory.js";
-import {addedColliders} from "../scenes/testRoom.js";
 import {addDivBlock} from "../scenes/roomUtils.js";
 
-export function addDragAndDrop(k, player, interactZone, map){
+export function addDragAndDrop(k, player, interactZone, map, addedCollisions){
     // const dropObject = document.getElementById(id)
     // console.log(dropObject)
     const dropObjectsHTML = document.getElementsByClassName("inventory-object")
@@ -47,11 +46,11 @@ export function addDragAndDrop(k, player, interactZone, map){
                     k.destroy(interactZone)
                     removeItem(inventory, draggedObject.id)
                     console.log("drop-moi ça")
-                    console.log(addedColliders)
+                    console.log(addedCollisions)
                     console.log(event)
                     // dropZone.append(dropObject)
                     dropZone.append(draggedObject)
-                    addDivBlock(k, addedColliders, "SpawnedBlock1")
+                    addDivBlock(k, addedCollisions, "SpawnedBlock1")
                     dropZone.innerHTML= ""
                     // let objectIndex = inventory.indexOf(dropObject)
                     // console.log(objectIndex)
@@ -85,6 +84,105 @@ export function addDragAndDrop(k, player, interactZone, map){
                     focusOnCanvas()
                     return;
                 }
+                else{
+                    console.log("mauvaise réponse")
+                    focusOnCanvas()
+                    return;
+                }
+                break;
+            case "InteractZoneTuto":
+                if (draggedObject.className.includes("div-block")){
+                    k.destroy(interactZone)
+                    removeItem(inventory, draggedObject.id)
+                    console.log("drop-moi ça")
+                    console.log(addedCollisions)
+                    console.log(event)
+                    // dropZone.append(dropObject)
+                    dropZone.append(draggedObject)
+                    addDivBlock(k, addedCollisions, "SpawnedBlockTuto")
+                    dropZone.innerHTML= ""
+                    // let objectIndex = inventory.indexOf(dropObject)
+                    // console.log(objectIndex)
+                    console.log(k.paused, player.paused)
+                    playAfterRightDrop(k, player)
+                    console.log("finish")
+                    focusOnCanvas()
+                    return
+                }
+                else{
+                    console.log("mauvaise réponse")
+                    focusOnCanvas()
+                    return;
+                }
+                break;
+            case "InteractZone1-1":
+                if (draggedObject.className.includes("div-block")){
+                    k.destroy(interactZone)
+                    removeItem(inventory, draggedObject.id)
+                    dropZone.append(draggedObject)
+                    addDivBlock(k, addedCollisions, "SpawnedBlock1-1")
+                    dropZone.innerHTML= ""
+                    playAfterRightDrop(k, player)
+                    focusOnCanvas()
+                    return
+                }
+                else{
+                    console.log("mauvaise réponse")
+                    focusOnCanvas()
+                    return;
+                }
+                break;
+            case "InteractZone1-2":
+                if (draggedObject.className.includes("div-block")){
+                    k.destroy(interactZone)
+                    removeItem(inventory, draggedObject.id)
+                    dropZone.append(draggedObject)
+                    addDivBlock(k, addedCollisions, "SpawnedBlock1-2")
+                    dropZone.innerHTML= ""
+                    playAfterRightDrop(k, player)
+                    focusOnCanvas()
+                    return
+                }
+                else{
+                    console.log("mauvaise réponse")
+                    focusOnCanvas()
+                    return;
+                }
+                break;
+            case "InteractZone1-3":
+                if (draggedObject.className.includes("div-block")){
+                    k.destroy(interactZone)
+                    removeItem(inventory, draggedObject.id)
+                    dropZone.append(draggedObject)
+                    addDivBlock(k, addedCollisions, "SpawnedBlock1-3")
+                    dropZone.innerHTML= ""
+                    playAfterRightDrop(k, player)
+                    focusOnCanvas()
+                    return
+                }
+                else{
+                    console.log("mauvaise réponse")
+                    focusOnCanvas()
+                    return;
+                }
+                break;
+            case "InteractZone1-4":
+                if (draggedObject.className.includes("div-block")){
+                    k.destroy(interactZone)
+                    removeItem(inventory, draggedObject.id)
+                    dropZone.append(draggedObject)
+                    addDivBlock(k, addedCollisions, "SpawnedBlock1-4")
+                    dropZone.innerHTML= ""
+                    playAfterRightDrop(k, player)
+                    focusOnCanvas()
+                    return
+                }
+                else{
+                    console.log("mauvaise réponse")
+                    focusOnCanvas()
+                    return;
+                }
+                break;
         }
 
     })
